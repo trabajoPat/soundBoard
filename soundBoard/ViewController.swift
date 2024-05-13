cb420a9c14a1d3bf21a5afd5e3fa8917e813a3fe
Farid <farid.gonzales@tecsup.edu.pe>
2024-05-13 17:04:55 -0500

    Initial Commit

---
+++ soundBoard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  soundBoard
+//
+//  Created by Farid Lucio Gonzales Gonzalo on 13/5/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
